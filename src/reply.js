57e97998245b892a90e0c405d943fdf9ff1b0cb9
Yuji Teshima <yuji.teshima@gmail.com>
2019-06-19 18:42:35 +0900

    modify reply.js

--- src/reply.js
+++ src/reply.js
@@ -3,19 +3,23 @@ import axios from 'axios'
 exports.handler = async function (event, context, callback) {
   const webhookBody = JSON.parse(event.body)
   console.log(webhookBody)
+
   const data = {
     replyToken: webhookBody.events[0].replyToken,
     messages: [{
-      type: 'text',
-      text: 'Hello Netlify Bot'
+      type: "text",
+      text: "Hello Netlify Bot"
     }]
   }
-  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+
+  const res = await axios.post("https://api.line.me/v2/bot/message/reply", data, {
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer${process.env.CHANNEL_TOKEN}`
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.CHANNEL_TOKEN}`
     }
   })
+  console.log(res)
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(event)
